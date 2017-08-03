db8e64e40a89659f545cfaba65a4fae094d94ca5
renaud23 <renaud.genevois@hotmail.fr>
2017-08-04 00:29:27 +0200

    divers

--- sandbox-react/src/main/webapp/WEB-INF/views/index.jsp
+++ sandbox-react/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,15 +1,10 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-
 <html>
 	<head>
 		<title>Bonjour !!!</title>
 	</head>
 	
 	<body>
-	
 		<div id="application"></div>
-	
-	
 		<script type="text/javascript" src="static/build/bundle.js"></script>
 	</body>
 </html>
--- sandbox-react/src/main/webapp/static/js/components/layout.js
+++ sandbox-react/src/main/webapp/static/js/components/layout.js
@@ -1,19 +1,20 @@
-import React, { Component, Children } from 'react';
+import React, { Component, Children } from "react";
 
 class Layout extends Component {
-
-    render() {
-        const { waiting } = this.props;
-        return (
-            <div className="layout">
-                { waiting ? <PreLoader /> : null }
-                { Children.only(this.props.children) }
-            </div>
-        );
-    }
+  render() {
+    const { waiting } = this.props;
+    return (
+      <div className="layout">
+        {waiting ? <PreLoader /> : null}
+        {Children.only(this.props.children)}
+      </div>
+    );
+  }
 }
 
-
-const PreLoader = () => (<div className="waiting"><img src="/sandbox-react/static/images/preloader.png" /></div>);
+const PreLoader = () =>
+  <div className="waiting">
+    <img src="/sandbox-react/static/images/preloader.png" />
+  </div>;
 
 export default Layout;
